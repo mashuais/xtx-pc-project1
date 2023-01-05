d2bcb1779e3f844a6b021d1c1dd532c86afa68f5
duoheshui <1731094567@qq.com>
2023-01-05 17:03:45 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <p>{{ $store.state.moduleB.username }}</p>
+    <p>{{ $store.getters['moduleB/newName'] }}</p>
+    <button @click="mutationsFn">mutationsFn</button>
+    <button @click="actionFn">actionFn</button>
+  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup() {
+    const store = useStore()
+    const mutationsFn = () => {
+      store.commit('moduleB/updateName')
     }
+    const actionFn = () => {
+      store.dispatch('moduleB/updateName')
+    }
+    return { mutationsFn, actionFn }
   }
 }
+</script>
+
+<style>
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,67 @@
 import { createStore } from 'vuex'
-
-export default createStore({
+// A模块
+const moduleA = {
   state: {
+    username: 'moduleA'
+  }
+}
+// B模块
+const moduleB = {
+  namespaced: true,
+  state: {
+    username: 'moduleB'
   },
   getters: {
+    changeName(state) {
+      return state.username + 'BBB'
+    }
   },
   mutations: {
+    updateName(state) {
+      state.username = state.username + 'bbbbb'
+    }
   },
   actions: {
-  },
+    updateName({ commit }) {
+      setTimeout(() => {
+        commit('updateName')
+      }, 2000)
+    }
+  }
+}
+export default createStore({
   modules: {
+    moduleA,
+    moduleB
   }
 })
+
+// export default createStore({
+//   state: {
+//     // 数据
+//     username: 'zs'
+//   },
+//   getters: {
+//     // vuex的计算属性
+//     newName(state) {
+//       return state.username + '!!!'
+//     }
+//   },
+//   mutations: {
+//     // 该数据函数
+//     updateName(state) {
+//       state.username = 'ls'
+//     }
+//   },
+//   actions: {
+//     // 请求数据函数
+//     updateName(ctx) {
+//       setTimeout(() => {
+//         ctx.commit('updateName')
+//       }, 1000)
+//     }
+//   },
+//   modules: {
+//     // 分模块
+//   }
+// })
